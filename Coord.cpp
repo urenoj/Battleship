68e42b87a3c8a500be51fef3706fed670f143686
urenoj <jureno95@gmail.com>
2014-12-06 11:22:16 -0800

    Create Coord.cpp

---
+++ Coord.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Coord.h"
+
+struct Ship {
+    Ship(std::string name, char rowBow, int colBow, char rowStern, int colStern) : Ship(name, Coord(rowBow, colBow), Coord(rowBow, rowStern)) { }
+    
+    Ship(std::string name, Coord bow, Coord stern) : name_(name), bow_(bow), stern_(stern) {
+        Coord min = bow_.min(stern_);
+        if (min != bow_) {
+            stern_ = bow_;
+            bow_ = min;
+        }
+    }
+    bool intersects(const Coord &coord) const {
+        int r = coord.row_, rb = bow_.row_, rs = stern_.row_;
+        int c = coord.col_, cb = bow_.col_, cs = stern_.row_;
+        
+        return (c == cb && r >= rb && r <= rs) || (r == rb && c >= cb && c <= cs);
+    }
+    bool intersects(const Ship &other) const {
+        int rb = bow_.row_, rs = stern_.row_;
+        int cb = bow_.col_, cs = stern_.col_;
+        int orb = other.bow_.row_, ors = other.stern_.row_;
+        int ocb = other.bow_.col_, ocs = other.stern_.col_;
+        
+        return (isVertical() && other.isVertical() && cb == ocb && rb >= rb >= orb && rb <= ors) || (isHorizontal() && other.isHorizontal() && rb == orb && cb >= ocb && cb <= ocs) || (isVertical() && other.isHorizontal() && rb <= orb && rs >= orb && cb >= ocb  && cb <= ocs) || (isHorizontal() && other.isVertical() && cb <= ocb && cs >= ocs && rb >= orb && rb <= ors);
+    }
+    bool isVertical() const { return bow_.sameCol(stern_); }
+    bool isHorizontal() const { return !isVertical(); }
+    
+private: std::string name_;
+    Coord bow_;
+    Coord stern_;
+};
