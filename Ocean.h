b4551c0b8c2efc5187e838353e2b1c3bd03f94d8
urenoj <jureno95@gmail.com>
2014-12-09 12:06:03 -0800

    Update Ocean.h

--- Ocean.h
+++ Ocean.h
@@ -1,30 +1,27 @@
-#ifndef Battleship_Ocean_h
-#define Battleship_Ocean_h
+#ifndef __Week14Labs__Ocean__
+#define __Week14Labs__Ocean__
 
 #include <iostream>
 #include <list>
 #include <vector>
-#include "Coord.h"
 #include "Ocean.h"
+#include "Coord.h"
+#include "Ship.h"
 
 struct Ocean {
-    Ocean();
-    
-    void addShips();
-    
-    bool overlaps(const std::string &shipName, char row1, int col1, char row2, int col2) const;
-    bool validCoord(const Coord &coord) const;
-    bool validShip(const std::string &shipName, char row1, int col1, char row2, int col2) const;
-    
-    void placeShip(const std::string &namePrompt, const std::string &shipName);
-    
-    friend std::ostream &operator << (std::ostream &os, const Ocean &ocean);
-    
-private:
-    static const char EMPTY = '*', SPACE = ' ';
-    static const int SIDE = 10;
-    char ocean_[SIDE][SIDE];
-    std::vector<Ship> ships_;
+  Ocean();
+  void addShips();
+  friend std::ostream& operator<<(std::ostream &os, const Ocean &ocean);
+  
+  bool overlaps (const std::string &shipName, char row1, int col1, char row2, int col2) const;
+  bool validCoord(const Coord& coord) const;
+  bool validShip(const std::string& shipName, char row1, int col1, char row2, int col2) const;
+  void placeShip(const std::string& namePrompt, const std::string& shipName);
+  
+  static const char EMPTY = '*', SPACE = ' ';
+  static const int SIDE = 10;
+  char ocean_[SIDE][SIDE];
+  std::vector<Ship> ships_;
 };
 
-#endif
+#endif /* defined(__Week14Labs__Ocean__) */
