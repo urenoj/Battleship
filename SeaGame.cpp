2d6c8b7a68e83ba965857efa9ec15b2f7370a2fd
urenoj <jureno95@gmail.com>
2014-12-11 22:10:13 -0800

    Update SeaGame.cpp

--- SeaGame.cpp
+++ SeaGame.cpp
@@ -1,14 +1,17 @@
 #include "SeaGame.h"
 
 void testSeaGame() {
-  SeaGame game;
-  game.play();
+    SeaGame game1;
+    SeaGame game2;
+    
+    game1.play();
+    game2.play();
 }
 
 SeaGame::SeaGame() {
-  std::cout << "Welcome to sea war game.\n";
+    std::cout << "Welcome to the Sea War Game.\n";
 }
 
 void SeaGame::play() {
-  ocean_.addShips();
+    ocean_.addShips();
 }
