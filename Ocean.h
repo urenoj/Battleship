800611d5fc9a314c1c33ab621476050f605c62ca
urenoj <jureno95@gmail.com>
2014-12-11 22:09:52 -0800

    Update Ocean.h

--- Ocean.h
+++ Ocean.h
@@ -9,19 +9,21 @@
 #include "Ship.h"
 
 struct Ocean {
-  Ocean();
-  void addShips();
-  friend std::ostream& operator<<(std::ostream &os, const Ocean &ocean);
+    Ocean();
+    void addShips();
+    friend std::ostream &operator<<(std::ostream &os, const Ocean &ocean);
   
-  bool overlaps (const std::string &shipName, char row1, int col1, char row2, int col2) const;
-  bool validCoord(const Coord& coord) const;
-  bool validShip(const std::string& shipName, char row1, int col1, char row2, int col2) const;
-  void placeShip(const std::string& namePrompt, const std::string& shipName);
+    bool overlaps (const std::string &shipName, char row1, int col1, char row2, int col2) const;
+    bool validCoord(const Coord &coord) const;
+    bool validShip(const std::string &shipName, char row1, int col1, char row2, int col2) const;
+    void placeShip(const std::string &namePrompt, const std::string &shipName);
   
-  static const char EMPTY = '*', SPACE = ' ';
-  static const int SIDE = 10;
-  char ocean_[SIDE][SIDE];
-  std::vector<Ship> ships_;
+    static const char EMPTY = '*', SPACE = ' ';
+    static const int SIDE = 10;
+    char ocean_[SIDE][SIDE];
+    std::vector<Ship> ships_;
+    
+    void hitShips();
 };
 
 #endif /* defined(__Week14Labs__Ocean__) */
